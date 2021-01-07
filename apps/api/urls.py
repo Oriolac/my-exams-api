3abe5e24cffb4ec848dfca35b44602e86a36fbd5
Marta Albets Mitjaneta <marta.albets@gmail.com>
2021-01-07 19:02:10 +0100

    Frontend schema done

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 from .views import *
+
+API_TITLE = 'Exam API'
+API_DESCRIPTION = 'A Web API for exams management'
+schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     path('exam/<int:pk>/grades/', ExamGrades.as_view()),
@@ -12,4 +18,6 @@ urlpatterns = [
     path('location/<int:pk>/', LocationDetail.as_view()),
     path('question/', QuestionList.as_view()),
     path('question/<int:pk>/', QuestionDetail.as_view()),
+    path('schema/', schema_view),
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
 ]
