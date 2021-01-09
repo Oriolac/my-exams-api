a06169c61b0ae11c573bd14074551703ecf74b70
Marta Albets Mitjaneta <marta.albets@gmail.com>
2021-01-09 11:18:13 +0100

    updated grade tests

--- apps/api/tests.py
+++ apps/api/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 from rest_framework.test import APIClient
 
-from apps.exams.models import Question, Choice, Exam, ExamLocation, Student
+from apps.exams.models import Question, Choice, Exam, ExamLocation, Student, Grade
 
 
 class QuestionTestCase(TestCase):
@@ -23,16 +23,27 @@ class QuestionTestCase(TestCase):
 class GradesTestCase(TestCase):
 
     def setUp(self) -> None:
-        exam = Exam.objects.create(title="My first exam", description="My first exam description",
-                                   date_start="2021-01-04T01:36:00Z", date_finish="2021-01-04T01:36:00Z")
         location = ExamLocation.objects.create(port=998, host="localhost", bind_key="string1")
+        exam = Exam.objects.create(title="My first exam", description="My first exam description",
+                                   date_start="2021-01-04T01:36:00Z", date_finish="2021-01-04T01:36:00Z",
+                                   location=location)
         question = Question.objects.create(title="First question?", correct_choice=1)
         choice = Choice.objects.create(choice_id=1, response="First choice")
-        student = Student.objects.create(studentID=123)
+        student = Student.objects.create(studentID="123")
+        grade = Grade.objects.create(student=student, exam=exam, correct=1)
 
         question.choices.set([choice])
-        exam.location.set(location)
         exam.questions.set([question])
         exam.students.set([student])
 
 
+    def test_put_grades(self):
+        client = APIClient()
+        data = {'correct': 1}
+        response = client.put('/api/exam/1/grades/123/', data)
+
+    """def test_get_grades(self):
+        client = APIClient()
+        response = client.get('/api/exam/1/grades/')
+        self.assertEquals(200, response.status_code)
+        print(response.json())"""
